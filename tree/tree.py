3bae855e524db402bd5ed106b45a6814dc291092
Vladyslav Okhrimenko <vladryk84@gmail.com>
2015-09-04 11:00:22 +0300

    Start work

---
+++ tree/tree.py
@@ -0,0 +1,16 @@
+
+class TreeNode(object):
+
+    def __init__(self, left, right, value):
+        self.left_node = left
+        self.right_node = right
+        self.value = value
+
+
+class TreeNodeManager(object):
+
+    def add_node(self):
+        pass
+
+    def remove_node(self):
+        pass
