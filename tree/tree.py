f78b71e3829d16bcf06c973088b4dc653aeb0406
Vladyslav Okhrimenko <vladryk84@gmail.com>
2015-09-09 17:50:26 +0300

    Writing 'remove' function

--- tree/tree.py
+++ tree/tree.py
@@ -1,10 +1,13 @@
 
 class TreeNode(object):
 
-    def __init__(self,  value, left=None, right=None):
+    def __init__(self,  value, left=None, right=None,
+                 parent=None, node_position=None):
         self.left_node = left
         self.right_node = right
         self.value = value
+        self.parent = parent
+        self.node_position = node_position
 
 
 class TreeNodeManager(object):
@@ -19,16 +22,30 @@ class TreeNodeManager(object):
         if value == node_value:
             print("This value is present")
         elif value > node_value:
-            node.right_node = TreeNode(value)
+            node.right_node = TreeNode(value, parent=node, node_position='right')
         elif value < node_value:
-            node.left_node = TreeNode(value)
+            node.left_node = TreeNode(value, parent=node, node_position='left')
 
     def remove_node(self, value):
         node = self.search_node(self.root_node, value)
+        right_node = getattr(node, 'right_node', None)
+        left_node = getattr(node, 'left_node', None)
+        position = getattr(node, 'node_position', None)
+        parent = getattr(node, 'parent', None)
         if value != getattr(node, 'value', None):
             print("value not found")
             return
-        if value == getattr(node, 'value', None):
+
+        if right_node and left_node is None:  # End of a node
+            if position == 'left':
+                setattr(parent, 'left_node', None)
+            elif position == 'right':
+                setattr(parent, 'right_node', None)
+
+        if right_node and left_node is not None:  # Node have a two nodes
+            pass
+
+        if right_node or left_node is not None:  # Node have a right or left node
             pass
 
     def create_tree(self, value):
